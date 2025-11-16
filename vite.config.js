70fc6b3b7a240fa019f468bd57cff7ec25ee7dea
charliesix6 <carlos.fincias.antolin@gmail.com>
2025-11-16 13:08:09 +0100

    fix: remove comment on base path in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "./", // usa './' para que los assets se resuelvan correctamente en GH Pages
+  base: "./",
   plugins: [react()],
 });
