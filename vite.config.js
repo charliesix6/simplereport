984ef6dab8905cdc35ffe1856a8b24f4c69cef99
charliesix6 <carlos.fincias.antolin@gmail.com>
2025-11-16 12:46:35 +0100

    prueba final

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  base: '/simplereport/',   // 👈 nombre EXACTO del repo en GitHub
+  build: {
+    outDir: 'docs',         // 👈 Vite generará la build en /docs
+  },
 })
